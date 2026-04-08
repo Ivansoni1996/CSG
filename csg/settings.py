8687cc728c1ac3b2548a5fb53a79ba1982e5cb03
ivanTsoni <southsunsoni@gmail.com>
2026-04-08 17:00:21 +0200

    addition of nav bars on each html pages and links

--- csg/settings.py
+++ csg/settings.py
@@ -107,7 +107,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
-LOGIN_REDIRECT_URL = 'liste_membres'
+LOGIN_REDIRECT_URL = 'verifier_role'
 LOGOUT_REDIRECT_URL = 'login'
 LOGIN_URL = 'login'
 
