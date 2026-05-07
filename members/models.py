715b9828b488e89d83458601c2109e8795d6cf52
ivanTsoni <southsunsoni@gmail.com>
2026-05-07 22:03:04 +0200

    creation du formulair de membre pas encore finalyser .

--- members/models.py
+++ members/models.py
@@ -1,20 +1,16 @@
 from django.db import models
-from django.contrib.auth.models import User
 
-class Membre(models.Model):
-    user = models.OneToOneField(User,on_delete=models.CASCADE, null=True, blank=True)
+class Members(models.Model):
     STATUS_ACTIVITE=[
-        ('actif', 'Àctif'),
+        ('actif', 'Actif'),
         ('inactif', 'Inactif'),
-        ('en_probation', 'En probation'),
-        ('default', ''),
-    ]
-    nom = models.CharField(max_length=100)
-    prenom = models.CharField(max_length=100)
-    email = models.EmailField(unique=False)
-    matricule= models.CharField(max_length=6,unique=True)
-    status= models.CharField(max_length=20,choices=STATUS_ACTIVITE, default='default')
-    cotisation = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+        ('en_probation', 'En probation'),]
+    member_id = models.CharField(max_length=30,primary_key=True,default="")
+    email = models.CharField(max_length=30,default="")
+    name = models.CharField(max_length=30,default="")
+    family_name=models.CharField(max_length=40,default="")
+    status=models.CharField(max_length=15, choices=STATUS_ACTIVITE, default="")
+    amount= models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
 
     def __str__(self):
-        return f"{self.prenom} {self.nom}"
+        return self.name
